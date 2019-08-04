99940dfb879b679dafec855fd60275be2cf4eb1e
Harshal <harshalnrn>
2019-08-04 08:58:24 +0530

    added Home component strcuture

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';            //'react is a package'
+import ReactDOM from 'react-dom';     //react-dom is a package
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+ReactDOM.render(<Home/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
