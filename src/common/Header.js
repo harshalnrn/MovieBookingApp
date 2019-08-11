23c1ecaff816dff55107b4e7d7b5114ebcdaaa6f
Harshal <harshalnrn>
2019-08-11 17:32:42 +0530

    Added Login form with username and password fields

--- src/common/Header.js
+++ src/common/Header.js
@@ -5,6 +5,45 @@ import logo from '../assets/logo.svg';
 import Modal from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
+//making use of styled JSX elements, in form of ready  components, from materialUI library
+
+
+const customStyles={
+
+content:{
+    top: '50%',
+    left:'50%',
+    right:'auto',
+    bottom:'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%,-50%)'
+
+}
+
+}
+//---------------------------------------------------------
+
+//below is stateless functional typrography component
+
+const TabContainer=function(props){
+
+    return (
+    <Typography component="div" style={{padding:0}} >
+{props.children}
+
+    </Typography>    /* note that inline styling has {{}} */
+    
+    );
+
+}
+
+
+//---------------------------------------------------------------
 
 // why cant we make use of JSX elements instead of importing these elements
 class Header extends Component {
@@ -18,20 +57,26 @@ this.state={
         
     }
 
+    //----------------------------------------
+
     openModalHandler=()=>{
 this.setState({modalIsOpen:true})
     }
 
+    //--------------------------------
     closeModal=()=>{
-
         this.setState({modalIsOpen:false})
     }
+    //----------------------------------
 
     tabChangeHandler=(event,position)=>{
         console.log(position);
         this.setState({ value:position });
+
+       // note: this value controls the display of underlining line on the tab
 }
 
+//---------------------------------------------
 render(){
     return(<div class="mainContainer">
         <header className="header">
@@ -40,15 +85,29 @@ render(){
     Login
     </Button>
     </header>
-    {/* display of modal is controlled via modalIsopen attribute */}
-    <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModal} className="loginRegModal">
+    {/* display of modal is controlled via modalIsopen attribute, similar to display attribute in html/css */}
+    <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModal} style={customStyles}>
     <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login" />   {/* position 0 */}
-                        <Tab label="Register" />  {/*  position1 */}
+                        <Tab label="Register"/>  {/*  position1 */}
+                        
                     </Tabs>
 
                     {/* Here onChange event handler for the Tabs,sets the state of value to 0 or 1, based upon position passed.
                     So is position an inbuilt property of Tab component*/}
+
+<TabContainer>
+<FormControl>
+<InputLabel htmlFor="username"> UserName</InputLabel>
+<Input id="username" type="text"/>
+</FormControl>
+<br/>
+<FormControl>
+<InputLabel htmlFor="password"> Password </InputLabel>
+<Input id="password" type="text"/>
+</FormControl>
+</TabContainer>
+
             
         </Modal>
     </div>);
