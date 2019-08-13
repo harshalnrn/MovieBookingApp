7984316a54d249f636583414987a6f057ddcd7a1
Harshal <harshalnrn>
2019-08-13 09:23:38 +0530

    added registeration form fields, with conditional display

--- src/common/Header.js
+++ src/common/Header.js
@@ -64,8 +64,18 @@ this.state={
     value:0,
     userName:"",
     passWord:"",
+    regUserName:"",
+    regPassword:"",
+    firstName:"",
+    lastName:"",
+    contactNo:"",
     userNameRequired:"dispNone",
-    passWordRequired:"dispNone"
+    passWordRequired:"dispNone",
+    ReguserNameRequired:"dispNone",
+    RegpassWordRequired:"dispNone",
+    firstNameRequired:"dispNone",
+    lastNameRequired:"dispNone",
+    contactNoRequired:"dispNone"
 }
         
     }
@@ -97,21 +107,51 @@ this.setState({userNameRequired:"dispNone"});
 
 this.state.passWord===""? this.setState({passWordRequired:"dispBlock"}):
 this.setState({passWordRequired:"dispNone"});
+
+
+// Question: Cant we do DOM manipulation here as we used to do in Javascript.(i.e manipulate displkay property of FormHelperText)
+}
+
+
+registerationClickHandler=()=>{
+    this.state.regUserName===""? this.setState({ReguserNameRequired:"dispBlock"}):
+    this.setState({ReguserNameRequired:"dispNone"});
+    
+    this.state.regPassword===""? this.setState({RegpassWordRequired:"dispBlock"}):
+    this.setState({RegpassWordRequired:"dispNone"});
+
+    this.state.firstName===""?this.setState({firstNameRequired:"dispBlock"}):
+    this.setState({firstNameRequired:"dispNone"});
+
+    this.state.lastName===""?this.setState({lastNameRequired:"displBlock"}):
+    this.setState({lastNameRequired:"dispNone"});
+
+    this.state.contactNo===""?this.setState({contactNo:"dispBlock"}):
+    this.setState({contactNo:"dispNone"});
 }
 
 
 handleClickOutside=()=>{
-
+//exercize1,2 bug
 this.setState({userNameRequired:"dispNone",
 passWordRequired:"dispNone" ,
+ReguserNameRequired:"dispNone",
+RegpassWordRequired:"dispNone",
+firstNameRequired:"dispNone",
+lastNameRequired:"dispNone",
+contactNoRequired:"dispNone",
+
 value:0});
 
 }
 
+
+
+//below are onchange events handlers for form
+
 inputUserNameChangeHandler=(e)=>{
     //setting userName field of state object, with form input's text value
     this.setState({userName:e.target.value});
-
 }
 
 
@@ -120,14 +160,44 @@ inputPasswordChangeHandler=(e)=>{
     //setting password field of state object, with form input's text value
     this.setState({passWord:e.target.value});
 
+}
 
+inputRegUserNameChangeHandler=(e)=>{
+    //setting userName field of state object, with form input's text value
+    this.setState({regUserName:e.target.value});
+}
+
+
+
+inputRegPasswordChangeHandler=(e)=>{
+    //setting password field of state object, with form input's text value
+    this.setState({regPassword:e.target.value});
+
+}
+
+inputFirstNameChangeHandler=(e)=>{
+
+this.setState({firstName:e.target.value});
+
+}
+
+inputLastNameChangeHandler=(e)=>{
+
+this.setState({lastName:e.target.value});
+    
+}
+
+inputContactChangeHandler=(e)=>{
+
+this.setState({contactNo:e.target.value});
+    
 }
 
 //------------------------------------------------------------------------------------------------------------------------
 render(){
     return(
     
-    <div class="mainContainer" >
+    <div className="mainContainer" >
 
 
         <header className="header">
@@ -155,6 +225,7 @@ So is position an inbuilt property of Tab component*/}
 <FormControl>
 <InputLabel htmlFor="username" > UserName</InputLabel>
 <Input id="username" type="text" username={this.state.userName} onChange={this.inputUserNameChangeHandler}/>
+{/* form helpertext with dynmic class */}
 <FormHelperText className={this.state.userNameRequired}><span className="red">Required</span></FormHelperText>
 </FormControl>
 <br/>
@@ -179,16 +250,44 @@ So is position an inbuilt property of Tab component*/}
 {this.state.value===1 &&
 
     <TabContainer>
+
     <FormControl>
-    <InputLabel > </InputLabel>
-    <Input id="" type="text"/>
+    <InputLabel htmlFor="firstName" > First Name</InputLabel>
+    <Input id="firstName" type="text"  onChange={this.inputFirstNameChangeHandler}/>
+    <FormHelperText className={this.state.firstNameRequired}><span className="red">Required</span></FormHelperText>
     </FormControl>
+
     <br/>
     <FormControl>
-    <InputLabel ></InputLabel>
-    <Input id="" type="text"/>
+    <InputLabel htmlFor="lastName"  > Last Name</InputLabel>
+    <Input id="lastName" type="text" onChange={this.inputLastNameChangeHandler}/>
+    <FormHelperText className={this.state.lastNameRequired}><span className="red">Required</span></FormHelperText>
+    </FormControl>
+
+
+{/* consider email and username as same for now */}
+    <br/>
+    <FormControl>
+    <InputLabel htmlFor="email"  > Email</InputLabel>
+    <Input id="email" type="text"  username={this.state.regUserName} onChange={this.inputRegUserNameChangeHandler}/>
+    <FormHelperText className={this.state.ReguserNameRequired}><span className="red">Required</span></FormHelperText>
+</FormControl>
+
+<br/>
+    <FormControl>
+    <InputLabel htmlFor="regPassword"  > Password</InputLabel>
+    <Input id="regPassword" type="password" password={this.state.regPassword} onChange={this.inputRegPasswordChangeHandler}/>
+    <FormHelperText className={this.state.RegpassWordRequired}><span className="red">Required</span></FormHelperText>
+</FormControl>
+<br/>
+
+    <FormControl>
+    <InputLabel htmlFor="contact"  > Contact No</InputLabel>
+    <Input id="contact" type="text" onChange={this.inputContactChangeHandler}/>
+    <FormHelperText className={this.state.contactNoRequired}><span className="red">Required</span></FormHelperText>
     </FormControl> <br/><br/>
-    <Button variant="contained" color="primary">REGISTER</Button>
+
+    <Button variant="contained" color="primary" onClick={this.registerationClickHandler}>REGISTER</Button>
     </TabContainer>
     }
             
